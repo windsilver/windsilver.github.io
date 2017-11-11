bdb2886b5558a76bce2b08f571b8e06fb97636ac
windsilver <33573367+windsilver@users.noreply.github.com>
2017-11-12 02:03:59 +0800

    1

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1510419532,
+	"version": 1510423406,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -10,11 +10,12 @@
 		"images/撞板-sheet0.png",
 		"images/物體-sheet0.png",
 		"images/牆壁-sheet0.png",
-		"images/over-sheet0.png",
+		"images/結束-sheet0.png",
 		"images/卡板-sheet0.png",
 		"images/布林開關-sheet0.png",
 		"images/方塊x-sheet0.png",
 		"images/方塊2-sheet0.png",
+		"images/開始鍵-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
