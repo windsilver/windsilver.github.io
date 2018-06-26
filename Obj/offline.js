ec87a2bb7e1c91538829b08506889f74ae28c9bf
windsilver <33573367+windsilver@users.noreply.github.com>
2018-06-27 03:29:49 +0800

    Updata
    1

---
+++ Obj/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1524462099,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/deadline-sheet0.png",
+		"images/platform-sheet0.png",
+		"images/platform-sheet1.png",
+		"images/indicator-sheet0.png",
+		"images/playeybox-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/rastart-sheet0.png",
+		"images/failed-sheet0.png",
+		"images/ready-sheet0.png",
+		"images/go-sheet0.png",
+		"images/debuff-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/up-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
